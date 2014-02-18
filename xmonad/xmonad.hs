44e570f7f881f3212268b273380e6dc90bfc4c1f
Michael Färber <01mf02@gmail.com>
2014-02-18 15:05:58 +0100

    Add xmonad configuration.

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,33 @@
+import XMonad
+import XMonad.Config.Desktop
+import XMonad.Config.Xfce
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.NoBorders
+import XMonad.Util.EZConfig
+
+main = xmonad $ xfceConfig
+	{ startupHook = startupHook xfceConfig >>
+	    -- make Java programs resize correctly by pretending we are a
+	    -- different WM (camouflage)
+	    setWMName "LG3D"
+
+	  -- inherit layout from desktopConfig
+	, layoutHook = desktopLayoutModifiers $
+	    -- do not show window borders in fullscreen mode
+	    smartBorders $
+	    -- switch only between two tiling algorithms instead of three
+	    -- as in the default configuration
+	    Tall 1 (3/100) (1/2) ||| Full
+	, terminal = "exo-open --launch TerminalEmulator"
+	}
+	`additionalKeys`
+	[ ((0       , xK_Print), spawn "xfce4-screenshooter -f")
+	, ((mod1Mask, xK_Print), spawn "xfce4-screenshooter -w")
+	, ((mod1Mask .|. shiftMask, xK_End), spawn "xfce4-session-logout --halt")
+	, ((mod4Mask, xK_f), spawn "exo-open --launch FileManager")
+	, ((mod4Mask, xK_w), spawn "exo-open --launch WebBrowser")
+	, ((mod4Mask, xK_m), spawn "exo-open --launch MailReader")
+	, ((mod4Mask, xK_b), spawn "transmission-gtk")
+	, ((mod4Mask, xK_p), spawn "xfce4-display-settings")
+	, ((mod4Mask, xK_s), spawn "xscreensaver-demo")
+	]
